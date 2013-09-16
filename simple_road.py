12f9a678d6adfb77228a605b93745df2b9f91e3f
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-09-16 17:32:38 -0400

    Update simple_road.py

--- simple_road.py
+++ simple_road.py
@@ -12,38 +12,37 @@ import car_engine
 engine = car_engine.CarEngine()
 L = 100.0
 maxv = 60.0
-maxa = 100.0
+maxa = 60.0
 totalT = 10.0
 
-def accelerate(x, v, t):
+def accelerate(x, v, t):    
     if x >= L:
-        a = 0
-        pedalF = engine.optimal_pedal_force(v, 0)
+        a = 0.
+        pedalF = 0.
     elif t>=0 and t<maxv/maxa:
         a = maxa
         pedalF = engine.optimal_pedal_force(v, a)
     elif x<L and x>L-0.5*maxv*maxv/maxa:
-        a = -maxa
-        pedalF = 0
+        a = - maxa * np.exp(-0.01*(totalT-t))
+        pedalF = 0.
     else:
-        a = 0
-        pedalF = engine.optimal_pedal_force(v, 0)
+        a = 0.
+        pedalF = 0.0
     return a, pedalF
 
-def deriv(xv, t):
-    if (xv[0] >= L and xv[1] < 0.0):
+def deriv(xvF, t):
+    if (xvF[0] >= L or xvF[1] < 0.0):
         return np.array([0.0, 0.0, 0.0])
     else:
-        a, pedalF = accelerate(xv[0], xv[1], t)
-        return np.array([max(0, xv[1]), a, pedalF])
+        a, pedalF = accelerate(xvF[0], xvF[1], t)
+        return np.array([max(0, xvF[1]), a, pedalF])
 
 def main():
     times = np.linspace(0.0, totalT, 101)
-    xvinit = np.array([0.0, 0.0, 0.0])
-    xvvals = odeint(deriv, xvinit, times)
-    for t, xv in zip(times, xvvals):
+    xvFinit = np.array([0.0, 0.0, 0.0])
+    xvFvals = odeint(deriv, xvFinit, times)
+    for t, xv in zip(times, xvFvals):
         print t, xv[1], xv[0], xv[2]
-        pass
 
 if __name__ == '__main__':
     main()
