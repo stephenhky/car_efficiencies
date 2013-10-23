f8490511b2ff1431c6c1bd17198a2bc9847dda3b
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-10-23 19:16:20 -0400

    Update car_engine.py

--- car_engine.py
+++ car_engine.py
@@ -5,6 +5,10 @@ Created on Fri Sep 13 12:04:24 2013
 @author: hok1
 """
 
+import math
+
+g = 1.
+
 class CarEngine:
     def __init__(self, m=1.0, b=1.0, c=0.5, max_force=100.0):
         self.m = m
@@ -15,12 +19,13 @@ class CarEngine:
     def airfriction(self, v):
         return self.b*v
     
-    def accelerate(self, pedal_force, v):
-        return min(max(0.0, (pedal_force-self.airfriction(v)-self.c)/self.m),
-                   self.max_force)
+    def accelerate(self, pedal_force, v, alpha=0):
+        return min(max(0.0,
+                       (pedal_force-self.airfriction(v)-self.c)/self.m)-g*math.sin(alpha),
+                       self.max_force)
         
-    def min_pedal_force(self, v):
-        return (self.airfriction(v)+self.c)
+    def min_pedal_force(self, v, alpha=0):
+        return (self.airfriction(v)+self.c+g*math.sin(alpha))
         
-    def optimal_pedal_force(self, v, a):
-        return (self.m*a+self.airfriction(v)+self.c)
+    def optimal_pedal_force(self, v, a, alpha=0):
+        return (self.m*a+self.airfriction(v)+self.c+g*math.sin(alpha))
