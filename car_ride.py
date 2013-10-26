35462580d1cc6bfae3dd14a3d5c164cd31c32f65
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-10-26 14:23:06 -0400

    debug in calling odeint, and add a test code

--- car_ride.py
+++ car_ride.py
@@ -26,8 +26,15 @@ class CarRide:
         return np.array([v, a])
         
     def solve_motion(self, timearray, init_x, init_v):
-        deriv = partial(self.derivative,
-                        time_array=timearray,
-                        acc_array=self.generate_accelerations(timearray))
-        xv_vals = odeint(deriv, np.array([init_x, init_v], timearray))
-        return xv_vals[:,0], xv_vals[:,1]
+        a_vals = self.generate_accelerations(timearray)        
+        deriv = partial(self.derivative, time_array=timearray, 
+                        acc_array=a_vals)
+        xv_vals = odeint(deriv, np.array([init_x, init_v]), timearray)
+        return xv_vals[:,0], xv_vals[:,1], a_vals
+        
+if __name__ == '__main__':
+    ride = CarRide()
+    time_array = np.linspace(0, 10, 101)
+    x_array, v_array, a_array = ride.solve_motion(time_array, 0., 10.)
+    for t, x, v, a in zip(time_array, x_array, v_array, a_array):
+        print t, x, v, a
