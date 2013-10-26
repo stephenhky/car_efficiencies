8241b9b638a6cdf070e18d5ffb7fa6a56a68757c
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-10-26 14:34:34 -0400

    add fuel efficiency analysis

--- car_ride.py
+++ car_ride.py
@@ -30,11 +30,39 @@ class CarRide:
         deriv = partial(self.derivative, time_array=timearray, 
                         acc_array=a_vals)
         xv_vals = odeint(deriv, np.array([init_x, init_v]), timearray)
-        return xv_vals[:,0], xv_vals[:,1], a_vals
+        F_vals = map(self.engine.optimal_pedal_force, xv_vals[:,1], a_vals)
+        return xv_vals[:,0], xv_vals[:,1], a_vals, F_vals
+        
+    def mpg(self, xarray, F_vals):
+        num_steps = len(xarray)
+        Farray = np.array(F_vals)
+
+        if num_steps <= 1:
+            return np.array([float('NaN')]), np.array([float('NaN')])
+        if num_steps > 1:
+            dxarray = []
+            dxarray.append((xarray[1]-xarray[0])*0.5)
+            for tstep in range(1, num_steps-1):
+                dxarray.append((xarray[tstep+1]-xarray[tstep-1])*0.5)
+            dxarray.append((xarray[-1]-xarray[-2])*0.5)
+            dxarray = np.array(dxarray)
+            
+            instwork = Farray*dxarray
+            
+            instMPG = instwork / dxarray
+            cumlMPG = []
+            for tstep in range(num_steps):
+                cumlMPG.append(sum(instwork[:(tstep+1)])/sum(dxarray[:(tstep+1)]))
+            cumlMPG = np.array(cumlMPG)
+            
+            return instMPG, cumlMPG
         
 if __name__ == '__main__':
     ride = CarRide()
     time_array = np.linspace(0, 10, 101)
-    x_array, v_array, a_array = ride.solve_motion(time_array, 0., 10.)
-    for t, x, v, a in zip(time_array, x_array, v_array, a_array):
-        print t, x, v, a
+    x_array, v_array, a_array, F_array = ride.solve_motion(time_array, 0., 10.)
+    inst_mpg_array, cuml_mpg_array = ride.mpg(x_array, F_array)
+    for t, x, v, a, inst_mpg, cuml_mpg in zip(time_array, x_array, v_array, 
+                                              a_array, inst_mpg_array,
+                                              cuml_mpg_array):
+        print t, x, v, a, inst_mpg, cuml_mpg
